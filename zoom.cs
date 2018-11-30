9dd4765eae2f849deb5392d55ecd9eb34b46236a
Luthere15 <42876725+Luthere15@users.noreply.github.com>
2018-11-30 14:04:55 -0500

    zoom & deckXML

---
+++ zoom.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class CameraZoom : Monobehavior
+{
+    [Header "set in inspector"]
+    public int zoom = 20;
+    public int normal = 60;
+    public float smooth = 5;
+
+    private bool isZoomed = false;
+
+    void Update()
+    {
+        if (input.GetMouseButtonDown(1))
+        {
+            isZoomed = !isZoomed;
+        
+        }
+
+        if( isZoomed)
+        {
+            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, zoom, Time.deltaTime * smooth);
+        }
+        else
+        {
+            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, normal, Time.deltaTime * smooth);
+        }
+    }
+	
+}
